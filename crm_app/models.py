8b12fe98c86381a9ff78e266c69ba039228153fe
proger-exe <elezor.2009@gmail.com>
2024-05-02 17:56:42 +0500

    ✅ Front-end, models, back-end auth copypast

--- crm_app/models.py
+++ crm_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Admin(models.Model):
+    username = models.TextField()
+    password = models.TextField()
+
+class Worker(models.Model):
+    username = models.TextField()
+    password = models.TextField()
+    name = models.TextField()
+    orders = models.JSONField()
+    position = models.TextField()
+
+class Sprint(models.Model):
+    name = models.TextField()
+    tasks = models.JSONField()
+
+class Order(models.Model):
+    slug = models.SlugField()
+    title = models.TextField()
+    amount = models.IntegerField()
+    client = models.IntegerField()
+
--- crm_app/views.py
+++ crm_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def login_site(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'base.html', {'error_message': 'Неверное имя пользователя или пароль.'})
+    return render(request, 'base.html')
+
+@login_required
+def logout_site(request):
+    logout(request)
+    return redirect('login')
